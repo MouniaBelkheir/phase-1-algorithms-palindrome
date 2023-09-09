9d2a044d4b9ce462085129312abac2000e0031f6
Mounia Belkheir <mouniabelkheir@outlook.com>
2023-09-09 14:17:16 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
+// Function to check if a string is a palindrome
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Convert the input string to lowercase
+  word = word.toLowerCase();
+  
+  // Remove non-letter characters (only keep letters)
+  word = word.replace(/[^a-z]/g, '');
+
+  // Initialize two pointers, one at the beginning (left) and one at the end (right) of the string.
+  let left = 0;
+  let right = word.length - 1;
+  
+  // Loop while the left pointer is less than or equal to the right pointer
+  while (left < right) {
+    // If the characters at the left and right pointers are not equal, return false
+    if (word[left] !== word[right]) {
+      return false;
+    }
+    // Move the left pointer one step to the right
+    left++;
+    // Move the right pointer one step to the left
+    right--;
+  }
+  
+  // If the loop completes without finding any unequal characters, return true
+  return true;
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
+// Test cases
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("robot")); // false
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("A man a plan a canal Panama")); // true
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
